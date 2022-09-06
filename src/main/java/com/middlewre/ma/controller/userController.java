7784ba03bccbbda6406bb2ca45722f987f0044df
jeewanthi1998 <jeewanthi.ch98@gmail.com>
2022-09-07 00:51:42 +0530

    First commit

---
+++ src/main/java/com/middlewre/ma/controller/userController.java
@@ -0,0 +1,19 @@
+package com.middlewre.ma.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+
+public class userController {
+
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "rrrrr";
+    }
+
+}
---
+++ src/test/java/com/middlewre/ma/MaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middlewre.ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
